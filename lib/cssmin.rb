393956a027a0ccbfcc9aa3128a9d2a579b478137
Dom Christie <christiedom@gmail.com>
2022-01-11 18:55:44 -0800

    $ bundle gem cssmin

---
+++ lib/cssmin.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cssmin/version"
+
+module Cssmin
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cssmin/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Cssmin
+  VERSION = "0.1.0"
+end
