fb0f4bf8e0dc481f97b8be1584034974a483a1ba
Dom Christie <christiedom@gmail.com>
2022-01-13 10:17:43 -0800

    Reorganise

---
+++ lib/cssmin/cssmin.rb
@@ -0,0 +1,16 @@
+require "execjs"
+
+MINIFIER_PATH = File.expand_path("../../csso/csso.js", __FILE__)
+MINIFIER_FUNCTION = "csso.minify"
+
+module Cssmin
+  class Error < StandardError; end
+
+  def self.minify(filename)
+    context.call(MINIFIER_FUNCTION, File.read(filename))["css"]
+  end
+
+  def self.context
+    @context ||= ExecJS.compile File.read(MINIFIER_PATH)
+  end
+end
