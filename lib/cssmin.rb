71a7ff864f7b66c2a7158111147f327f261bc13b
Dom Christie <christiedom@gmail.com>
2022-01-13 10:17:43 -0800

    API for minifying CSS

--- lib/cssmin.rb
+++ lib/cssmin.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
+require "execjs"
 require_relative "cssmin/version"
+
+MINIFIER_PATH = File.expand_path("../csso/csso.js", __FILE__)
+MINIFIER_FUNCTION = "csso.minify"
 
 module Cssmin
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.minify(filename)
+    context.call(MINIFIER_FUNCTION, File.read(filename))["css"]
+  end
+
+  def self.context
+    @context ||= ExecJS.compile File.read(MINIFIER_PATH)
+  end
 end
--- test/test_cssmin.rb
+++ test/test_cssmin.rb
@@ -6,4 +6,9 @@ class TestCssmin < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Cssmin::VERSION
   end
+
+  def test_it_minifies_a_css_file
+    test_file = File.expand_path("../fixture.css", __FILE__)
+    assert_equal "body{color:#313373}", ::Cssmin.minify(test_file)
+  end
 end
